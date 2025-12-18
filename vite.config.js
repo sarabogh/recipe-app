4e34ee07aaf3053b1a9e7523eba66eb1a3f268e0
Sara <sarabogh22@gmail.com>
2025-12-18 00:58:09 -0700

    doble checking

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/recipe-app/', // <-- replace with your repo name
+  base: '/recipe-app/', // <- this MUST match your repo name
 })
